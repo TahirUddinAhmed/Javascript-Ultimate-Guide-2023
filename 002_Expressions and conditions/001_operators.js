323baf0188b07f5c1c4f738a908f26f97bab9f67
Tahir Ahmed <01tahirahmed@gmail.com>
2023-07-25 13:35:27 +0530

    operators in JS with NOTES

---
+++ 002_Expressions and conditions/001_operators.js
@@ -0,0 +1,26 @@
+let a = 32;
+let b = 3;
+console.log("a + b= ", a + b);
+console.log("a - b= ", a - b);
+console.log("a * b= ", a * b);
+console.log("a / b= ", a / b);
+console.log("a % b= ", a % b);
+console.log("a ** b= ", a ** b); // exponential operator
+
+// increment and decrement operator
+// (++, --) pre-increment , post-increment
+console.log("a++ = ", a++); // pre-increment
+console.log("a = ", a);
+console.log("++b=", ++b); // post-increment
+
+
+// what will be output of the following code 
+console.log("Quick Quiz:- ");
+let x = 5;
+
+console.log("++x = ", ++x);
+console.log("x++ = ", x++);
+console.log("--x = ", --x);
+console.log("x-- = ", x--);
+console.log("x = ", x);
+console.log("x--", x--);
