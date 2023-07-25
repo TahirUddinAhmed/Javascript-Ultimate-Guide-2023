79e27a8a4766cd02abf9115261d301e4f3f84c3b
Tahir Ahmed <01tahirahmed@gmail.com>
2023-07-25 10:27:25 +0530

    Varibles in Javascript with NOTES

---
+++ 00_Intoduction/002_variables.js
@@ -0,0 +1,17 @@
+var a = 97; // here a is "a" is a variable which contains value 97
+// we can also declare a variable using the let keyword
+let x = 120;
+console.log(x);
+x = "peter";
+console.log(x); // value of js variable can be change during the execution of a program
+
+// let 8num = 90; // not allowed
+let $name = "tahir"; // allowed
+// let var = 56.54; // not allowed
+
+let num = 12;
+let Num = 13; // here num and Num are two different variables
+
+
+
+
