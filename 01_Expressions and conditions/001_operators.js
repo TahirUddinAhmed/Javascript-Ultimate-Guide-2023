6b1e92995178541b3a160f1722302ebc5674c084
Tahir Ahmed <01tahirahmed@gmail.com>
2023-07-25 17:44:52 +0530

    Operators in JS

--- 01_Expressions and conditions/001_operators.js
+++ 01_Expressions and conditions/001_operators.js
@@ -1,26 +1,39 @@
 let a = 32;
 let b = 3;
-console.log("a + b= ", a + b);
-console.log("a - b= ", a - b);
-console.log("a * b= ", a * b);
-console.log("a / b= ", a / b);
-console.log("a % b= ", a % b);
-console.log("a ** b= ", a ** b); // exponential operator
+// console.log("a + b= ", a + b);
+// console.log("a - b= ", a - b);
+// console.log("a * b= ", a * b);
+// console.log("a / b= ", a / b);
+// console.log("a % b= ", a % b);
+// console.log("a ** b= ", a ** b); // exponential operator
 
 // increment and decrement operator
 // (++, --) pre-increment , post-increment
-console.log("a++ = ", a++); // pre-increment
-console.log("a = ", a);
-console.log("++b=", ++b); // post-increment
+// console.log("a++ = ", a++); // pre-increment
+// console.log("a = ", a);
+// console.log("++b=", ++b); // post-increment
 
 
 // what will be output of the following code 
-console.log("Quick Quiz:- ");
+// console.log("Quick Quiz:- ");
 let x = 5;
 
-console.log("++x = ", ++x);
-console.log("x++ = ", x++);
-console.log("--x = ", --x);
-console.log("x-- = ", x--);
-console.log("x = ", x);
-console.log("x--", x--);
+// console.log("++x = ", ++x);
+// console.log("x++ = ", x++);
+// console.log("--x = ", --x);
+// console.log("x-- = ", x--);
+// console.log("x = ", x);
+// console.log("x--", x--);
+
+// Assignment Operator 
+let num = 12;
+num += 5;  // num = num + 12
+// console.log(num);
+
+// comparision operator
+let p = 5;
+let q = '5';
+console.log(p == q);
+console.log(p != q);
+console.log(p === q);
+console.log(p != q);
