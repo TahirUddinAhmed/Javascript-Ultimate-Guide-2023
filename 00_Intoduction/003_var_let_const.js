20d8ea31b5242702dd8047ccba0ddef95ebc6046
Tahir Ahmed <01tahirahmed@gmail.com>
2023-07-25 10:53:16 +0530

    let const and var

---
+++ 00_Intoduction/003_var_let_const.js
@@ -0,0 +1,20 @@
+let a= "tahir";
+// let a = 890; // NOT Allowed
+
+var b = 56;
+b = 100; // updated
+var b = "John" // re-declared
+console.log(b);
+var c = null;
+
+const CEO = "Mark Zukarbarg";
+// CEO = "Tahir"; // const can't be updated
+// let CEO = "JOHN"; // not allowed // can't be re-declared
+console.log(CEO);
+
+{
+    let a = 45;
+    console.log(a);
+}
+
+console.log(a);
